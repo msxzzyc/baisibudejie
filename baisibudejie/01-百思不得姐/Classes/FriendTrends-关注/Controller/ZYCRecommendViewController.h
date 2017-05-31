cb4d2fc8812894fd1f3726bad7ec05bc2d226834
zyc <10168363742qq.com>
2017-05-31 23:46:01 +0800

    添加了框架 sv hud 、afn和sdwebimage   创建了ZYCRecommendViewController

---
+++ baisibudejie/01-百思不得姐/Classes/FriendTrends-关注/Controller/ZYCRecommendViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZYCRecommendViewController.h
+//  01-百思不得姐
+//
+//  Created by wpzyc on 2017/5/31.
+//  Copyright © 2017年 wpzyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYCRecommendViewController : UIViewController
+
+@end
