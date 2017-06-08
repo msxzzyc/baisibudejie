2d1b8868e20b60edab25491c2feecfd6b67fef70
zyc <10168363742qq.com>
2017-06-08 16:54:03 +0800

    使用runtime设置占位文字和光标颜色

--- baisibudejie/01-百思不得姐/Classes/FriendTrends-关注/ZYCTextField.h
+++ baisibudejie/01-百思不得姐/Classes/FriendTrends-关注/ZYCTextField.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface ZYCTextField : UITextField
-
+/** 占位文字颜色 */
+//@property(nonatomic,strong)UIColor *placeholderColor;
 @end
--- baisibudejie/01-百思不得姐/Classes/FriendTrends-关注/ZYCTextField.m
+++ baisibudejie/01-百思不得姐/Classes/FriendTrends-关注/ZYCTextField.m
@@ -7,17 +7,83 @@
 //
 
 #import "ZYCTextField.h"
+#import <objc/runtime.h>
 
+static NSString *const ZYCPlaceholderColorKeyPath = @"_placeholderLabel.textColor";
 @implementation ZYCTextField
 
--(void)drawPlaceholderInRect:(CGRect)rect
+
+//-(void)drawPlaceholderInRect:(CGRect)rect
+//{
+//    [self.placeholder drawInRect:CGRectMake(0, 15, self.width, 25) withAttributes:@{NSForegroundColorAttributeName : [UIColor grayColor],NSFontAttributeName : self.font }];
+//    
+//}
+
+/**
+ 运行时（Runtime）：
+ *苹果官方的一套c语言库
+ *能做很多底层操作（比如访问隐藏的一些成员变量\成员方法......）
+ */
++ (void)initialize
 {
-    [self.placeholder drawInRect:CGRectMake(0, 15, self.width, 25) withAttributes:@{
-                                                                                    NSForegroundColorAttributeName : [UIColor grayColor],NSFontAttributeName : self.font }];
+    unsigned int count = 0;
+    //拷贝出所有成员变量列表
+    Ivar *ivars = class_copyIvarList([UITextField class], &count);
+    for (int i = 0; i < count; i++) {
+        //取出成员变量
+        Ivar ivar = *(ivars + i);
+        //打印成员变量名字
+        ZYCLog(@"%s",ivar_getName(ivar));
+    }
     
+    //释放内存
+    free(ivars);
 }
 
 
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+//    UILabel *placeholderLabel = [self valueForKeyPath:@"_placeholderLabel"];
+//    
+//    placeholderLabel.textColor = [UIColor redColor];
+    
+    //设置光标颜色和占位文字颜色一致
+    self.tintColor = self.textColor;
+    //不成为第一响应者
+    [self resignFirstResponder];
+}
+/**
+ *当前文本框聚焦时会调用
+ */
+- (BOOL)becomeFirstResponder
+{
+    //修改占位文字颜色
+    [self setValue:[UIColor whiteColor] forKeyPath:ZYCPlaceholderColorKeyPath];
+    return [super becomeFirstResponder];
+    
+}
+/**
+ *当前文本框失去焦点时会调用
+ */
+- (BOOL)resignFirstResponder
+{
+    //修改占位文字颜色
+    [self setValue:[UIColor grayColor] forKeyPath:ZYCPlaceholderColorKeyPath];
+    return [super resignFirstResponder];
+    
+    
+}
 
+//- (void)setPlaceholderColor:(UIColor *)placeholderColor
+//{
+//    
+//    _placeholderColor = placeholderColor;
+//    
+//    //修改占位文字颜色
+//    [self setValue:placeholderColor forKeyPath:ZYCPlaceholderColorKeyPath];
+//    
+//}
 
 @end
