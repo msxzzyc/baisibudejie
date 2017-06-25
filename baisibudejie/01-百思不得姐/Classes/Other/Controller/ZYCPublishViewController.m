0c12fe44cb6cd8546ce3464ccc0a24c30aaa8f56
zyc <10168363742qq.com>
2017-06-25 14:40:40 +0800

    利用pop实现按钮和标语的弹簧动画

--- baisibudejie/01-百思不得姐/Classes/Other/Controller/ZYCPublishViewController.m
+++ baisibudejie/01-百思不得姐/Classes/Other/Controller/ZYCPublishViewController.m
@@ -9,6 +9,8 @@
 #import "ZYCPublishViewController.h"
 #import "ZYCVerticalButton.h"
 #import <POP.h>
+static CGFloat const ZYCAnimationDelay = 0.1;
+static CGFloat const ZYCSpringFactor = 6;
 @interface ZYCPublishViewController ()
 
 @end
@@ -18,11 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
    
-    //添加slogan(标语)
-    UIImageView *slogan = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"app_slogan"]];
-    slogan.centerX = ZYCScreenW *0.5;
-    slogan.centerY = ZYCScreenH *0.2;
-    [self.view addSubview:slogan];
+    
     
     
     //数据
@@ -57,12 +55,32 @@
         CGFloat buttonStartY = buttonEndY - ZYCScreenH;
 //        button.frame = CGRectMake(buttonX, buttonY, buttonW, buttonH);
         
-        //添加动画
+        //为按钮添加动画
         POPSpringAnimation *anim = [POPSpringAnimation animationWithPropertyNamed:kPOPViewFrame];
         
         anim.fromValue = [NSValue valueWithCGRect:CGRectMake(buttonX, buttonStartY, buttonW, buttonH)];
         anim.toValue = [NSValue valueWithCGRect:CGRectMake(buttonX, buttonEndY, buttonW, buttonH)];
+        anim.springBounciness = ZYCSpringFactor;
+        anim.springSpeed = ZYCSpringFactor;
+        anim.beginTime = CACurrentMediaTime() + ZYCAnimationDelay*i;
         [button pop_addAnimation:anim forKey:nil];
+        
+        //添加slogan(标语)
+        UIImageView *slogan = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"app_slogan"]];
+        CGFloat sloganCenterX = ZYCScreenW *0.5;
+        
+        CGFloat sloganCenterEndY = ZYCScreenH *0.2;
+        CGFloat sloganCenterStartY = sloganCenterEndY - ZYCScreenH;
+        [self.view addSubview:slogan];
+        
+        //为slogan添加动画
+        POPSpringAnimation *sloganAnim = [POPSpringAnimation animationWithPropertyNamed:kPOPViewCenter];
+        sloganAnim.fromValue = [NSValue valueWithCGPoint:CGPointMake(sloganCenterX, sloganCenterStartY)];
+        sloganAnim.toValue = [NSValue valueWithCGPoint:CGPointMake(sloganCenterX, sloganCenterEndY)];
+        anim.springBounciness = ZYCSpringFactor;
+        anim.springSpeed = ZYCSpringFactor;
+        sloganAnim.beginTime = CACurrentMediaTime() + ZYCAnimationDelay*images.count;
+        [slogan pop_addAnimation:sloganAnim forKey:nil];
         
     }
     
@@ -71,7 +89,14 @@
     
     [self dismissViewControllerAnimated:NO completion:nil];
 }
-
+/**
+ pop和Core Animation的区别
+ 1.Core Animation的动画只能添加到layer上
+ 2.pop的动画能添加到任何对象
+ 3.pop的底层并非基于Core Animation, 是基于CADisplayLink
+ 4.Core Animation的动画仅仅是表象, 并不会真正修改对象的frame\size等值
+ 5.pop的动画实时修改对象的属性, 真正地修改了对象的属性
+ */
 
 /*
 #pragma mark - Navigation
